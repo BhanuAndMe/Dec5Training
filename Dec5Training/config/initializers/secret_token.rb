e80c2de0ceb150ffa6578ddecb2016c59120dcf1
Barun Thapa <barunthapa.bvdt@gmail.com>
2012-12-06 10:57:27 +0545

    Add Dec5Training Project

---
+++ Dec5Training/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dec5Training::Application.config.secret_token = '46923b192ef99dba9000e538957ac9573b9c1ca5b2fef415bb7a49dc8e2572c9f1166bb6bd088525f802830dad248bbc20888c19dc55c0f4bc689b23ce80b32b'
